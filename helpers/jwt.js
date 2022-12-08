e2069a39057bfb38e6363dbcd3a3dd16a3de3243
ramonantoniosantoscuevas15 <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2022-12-08 16:23:29 -0400

    aplicando sockets

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -18,7 +18,19 @@ const generalJWT = (uid) =>{
     });
 
 }
+const comprobarJWT = (token = '')=>{
+    try {
+        const { uid} = jwt.verify(token, process.env.JWT_KEY);
+        return [true,uid];
+        
+    } catch (error) {
+        return [false,null]
+        
+    }
+
+}
 
 module.exports = {
-    generalJWT
+    generalJWT,
+    comprobarJWT
 }
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,10 +1,18 @@
+const { comprobarJWT } = require('../helpers/jwt');
 const {io} = require('../index');
+const {usuarioConectado,usuarioDesconectado} = require('../controllers/socket');
 //mensajes de sockets
-io.on('connection', client => {
+io.on('connection', (client) => {
     console.log('Cliente conectado');
+    const [valido,uid]= comprobarJWT(client.handshake.headers['x-token'])
+    //verificar autenticacion
+    if(!valido){return client.disconnect();}
+    //cliente autenticado
+    usuarioConectado(uid);
+    
     
     client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+        usuarioDesconectado(uid);
      });
     
    // client.on('mensaje', ( payload)=>{
